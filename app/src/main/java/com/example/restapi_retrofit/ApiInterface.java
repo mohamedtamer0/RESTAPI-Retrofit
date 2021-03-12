a1b5f4d68fb70c72119ecad375885cee4f204d76
mohamed tamer0 <mt4452242@gmail.com>
2021-03-13 01:19:50 +0200

    POST Request @Body

--- app/src/main/java/com/example/restapi_retrofit/ApiInterface.java
+++ app/src/main/java/com/example/restapi_retrofit/ApiInterface.java
@@ -1,14 +1,18 @@
 package com.example.restapi_retrofit;
 
-import java.util.List;
-
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
 
-    @GET("posts")
-    public Call<List<Post>> getPost(@Query("userId")String userId);
+    @GET("posts/id")
+    public Call<Post> getPost(@Path("id") int postid);
+
+    @POST("posts")
+    public Call<Post> storePost(@Body Post post);
 
 }
--- app/src/main/java/com/example/restapi_retrofit/MainActivity.java
+++ app/src/main/java/com/example/restapi_retrofit/MainActivity.java
@@ -5,25 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.POST;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView PostTitleTV;
-
+    TextView PostBodyTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PostTitleTV = findViewById(R.id.post_title_tv);
+        Post post = new Post(5,"CODEDEV", "Hello World");
 
+        PostTitleTV = findViewById(R.id.post_title_tv);
+        PostBodyTV = findViewById(R.id.post_body_tv);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/").addConverterFactory(GsonConverterFactory.create())
                 .build();
@@ -32,18 +33,19 @@ public class MainActivity extends AppCompatActivity {
         ApiInterface apiInterface = retrofit.create(ApiInterface.class);
 
 
-        Call<List<Post>> call = apiInterface.getPost("1");
+        Call<Post> call = apiInterface.storePost(post);
 
-        call.enqueue(new Callback<List<Post>>() {
+        call.enqueue(new Callback<Post>() {
             @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                PostTitleTV.setText(response.body().get(0).getTitle());
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                PostTitleTV.setText(response.body().getTitle());
+                PostBodyTV.setText(response.body().getBody());
             }
 
             @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
+            public void onFailure(Call<Post> call, Throwable t) {
                 PostTitleTV.setText(t.getMessage());
-
+                PostBodyTV.setText(t.getMessage());
             }
         });
 
